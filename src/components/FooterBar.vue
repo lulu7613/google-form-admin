eb828b21aa88610e0fc805d3c7fb7e18184f765f
Lulu Tsai <lulu_tsai@myplay20.com>
2020-06-16 13:14:31 +0800

    完成 login 底部切版 (以元件方式顯示)

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="mt-3 px-2 d-flex justify-content-between" style="font-size: .9rem;">
+    <div style="font-weight: 600;">
+      繁體中文
+    </div>
+    <ul class="d-flex justify-content-between" style="width: 35%">
+      <li class="cursor-point">說明</li>
+      <li class="cursor-point">隱私權</li>
+      <li class="cursor-point">條款</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'FooterBar',
+
+  components: {
+  },
+
+  data() {
+    return {
+      account: '',
+      isFocus: false,
+      hasData: false,
+    };
+  },
+
+  watch: {
+  },
+
+  methods: {
+  },
+
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,34 @@
 <template>
   <div class="home">
-    <div class="login-card mx-auto text-center">
-      <span class="logo">Google</span>
-      <h1>登入</h1>
-      <p>使用您的 Google 帳戶</p>
-      <Input
-        v-model="account"
-        type="text"
-        :auto-focus="true"
-        placeholder="電子郵件地址或電話"
-        class="mt-4"
-      />
-      <div class="text-left text-primary cursor-point mt-3">忘記電子郵件地址？</div>
-      <div
-        class="mt-5 text-left"
-        style="line-height: 1.7rem;"
-      >
-        如果這不是你的電腦，請使用訪客模式以私密方式登入。
-        <span class="text-primary cursor-point">瞭解詳情</span>
+    <div class="mx-auto" style="width: 448px; height: 550px">
+      <div class="login-card text-center">
+        <span class="logo">Google</span>
+        <h1>登入</h1>
+        <p>使用您的 Google 帳戶</p>
+        <Input
+          v-model="account"
+          type="text"
+          :auto-focus="true"
+          placeholder="電子郵件地址或電話"
+          class="mt-4"
+        />
+        <div class="text-left text-primary cursor-point mt-3">忘記電子郵件地址？</div>
+        <div
+          class="mt-5 text-left"
+          style="line-height: 1.7rem;"
+        >
+          如果這不是你的電腦，請使用訪客模式以私密方式登入。
+          <span class="text-primary cursor-point">瞭解詳情</span>
+        </div>
+        <div class="footer mt-5 d-flex justify-content-between">
+          <Popover title="建立帳戶">
+            <div @click="test">建立個人帳戶</div>
+            <div>建立管理業務帳戶</div>
+          </Popover>
+          <Button type="primary">繼續</Button>
+        </div>
       </div>
-      <div class="footer mt-5 d-flex justify-content-between">
-        <Popover title="建立帳戶">
-          <div @click="test">建立個人帳戶</div>
-          <div>建立管理業務帳戶</div>
-        </Popover>
-        <Button type="primary">繼續</Button>
-      </div>
+      <FooterBar />
     </div>
   </div>
 </template>
@@ -34,6 +37,7 @@
 import Input from '@/components/Input.vue';
 import Button from '@/components/Button.vue';
 import Popover from '@/components/Popover.vue';
+import FooterBar from '@/components/FooterBar.vue';
 
 export default {
   name: 'Home',
@@ -42,24 +46,16 @@ export default {
     Input,
     Button,
     Popover,
+    FooterBar,
   },
 
   data() {
     return {
       account: '',
-      isFocus: false,
-      hasData: false,
     };
   },
 
   watch: {
-    account(value) {
-      if (value) {
-        this.hasData = true;
-      } else {
-        this.hasData = false;
-      }
-    },
   },
 
   methods: {
@@ -75,7 +71,7 @@ export default {
 
 <style lang="scss" scoped>
 .login-card {
-    width: 448px;
+    width: 100%;
     height: 500px;
     padding: 48px 40px 36px;
     box-sizing: border-box;
