7a46d695966942269beb20bfe561e01dd25f2b7c
Lulu Tsai <lulu_tsai@myplay20.com>
2020-06-16 09:19:38 +0800

    將 home.scss 內容移到 Home.vue，方便管理

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="home">
-    <div class="login-card mx-auto">
+    <div class="login-card mx-auto text-center">
       <span class="logo">Google</span>
       <h1>登入</h1>
       <p>使用您的 Google 帳戶</p>
-      <Input v-model="account" type="text" placeholder="電子郵件地址或電話" />
-      <div class="footer">
+      <Input v-model="account" type="text" placeholder="電子郵件地址或電話" class="mt-4" />
+      <div class="text-left text-primary cursor-point mt-3">忘記電子郵件地址？</div>
+      <div
+        class="mt-5 text-left"
+        style="line-height: 1.7rem;"
+      >
+        如果這不是你的電腦，請使用訪客模式以私密方式登入。
+        <span class="text-primary cursor-point">瞭解詳情</span>
+      </div>
+      <div class="footer mt-5">
+        建立帳戶
+        繼續
       </div>
     </div>
   </div>
@@ -50,3 +60,23 @@ export default {
 
 };
 </script>
+
+<style lang="scss" scoped>
+.login-card {
+    width: 448px;
+    height: 500px;
+    padding: 48px 40px 36px;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    margin-top: 20vh;
+
+    .logo {
+        font-size: 1.8rem;
+    }
+
+    h1 {
+        font-size: 1.5rem;
+    }
+}
+</style>
