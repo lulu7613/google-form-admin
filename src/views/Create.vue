3f64376aa9be71819c7fdccef022ef0cd82f933f
Lulu Tsai <lulu_tsai@myplay20.com>
2020-06-19 15:54:06 +0800

    完成 Create.vue form

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -39,15 +39,52 @@
               >
                 <template v-slot:email>@gmail.com</template>
               </Input>
-              <div v-show="!isError" class="mt-2" style="font-size: .9rem;">
+              <div v-show="form.user_name !== null" class="mt-2" style="font-size: .9rem;">
                 您可以使用英文字母、數字和半形句號
               </div>
               <div class="mt-2 text-primary cursor-point" style="font-size: .9rem;">
                 改為使用我目前的電子郵件地址
               </div>
             </div>
+            <div class="mt-4 d-flex justify-content-between">
+              <div style="width: 42%">
+                <Input
+                  size="small"
+                  :type="passType"
+                  v-model="form.pass"
+                  placeholder="密碼"
+                  errorMsg="輸入密碼"
+                  :isError="form.pass === null"
+                />
+              </div>
+              <div style="width: 42%">
+                <Input
+                  size="small"
+                  :type="passType"
+                  v-model="form.check_pass"
+                  placeholder="確認"
+                  errorMsg="確認密碼"
+                  :isError="form.check_pass === null"
+                />
+              </div>
+              <div
+                ref="pass-icon"
+                class="cursor-point align-self-center"
+                @click="actTogglePassIcon"
+              >
+                <svg-icon v-show="!showPass" icon-class="eye-show" size="25px" />
+                <svg-icon v-show="showPass" icon-class="eye-close" size="25px" />
+              </div>
+            </div>
+            <div v-show="form.pass !== null && form.check_pass !== null" class="mt-2" style="font-size: .9rem;">
+              請混合使用 8 個字元以上的英文字母、數字和符號
+            </div>
           </div>
-          <div>
+          <div class="mt-5 d-flex justify-content-between">
+            <div class="text-left text-primary cursor-point mt-3" @click="redirectLogin">
+              <!-- {{ $t('pass_forgot', $store.state.lang) }} -->
+              請改為登入帳戶
+            </div>
             <Button type="primary" @click="redirect">
               {{ $t('next', $store.state.lang) }}
             </Button>
@@ -96,7 +133,8 @@ export default {
         pass: '',
         check_pass: '',
       },
-      isError: false,
+      passType: 'password',
+      showPass: false,
     };
   },
 
@@ -104,6 +142,21 @@ export default {
     redirect() {
       this.form.first_name = null;
       this.form.user_name = null;
+      this.form.pass = null;
+    },
+
+    redirectLogin() {
+      this.$router.push('/login')
+    },
+
+    actTogglePassIcon() {
+      this.showPass = !this.showPass;
+
+      if (this.showPass) {
+        this.passType = 'text';
+      } else {
+        this.passType = 'password';
+      }
     },
   },
 };
